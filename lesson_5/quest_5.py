1a3c3c8b8ecf38424a4da12e3970f05db4a7d333
EchoDroid <tselukovsky.ivan@gmail.com>
2020-04-22 23:41:12 +1100

    Промежуточный коммит 2 (для дальнейшего слияния) Выполнены 4, 5, 6 задания

--- lesson_5/quest_5.py
+++ lesson_5/quest_5.py
@@ -1,2 +1,22 @@
 """ 5) Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран. """
+
+
+from random import randint
+
+file_name = 'quest_5.txt'
+gen_num_list = [str(randint(1, 100)) for _ in range(20)]
+int_list = []
+with open(file_name, 'w') as f_obj:
+    print(' '.join(gen_num_list), file=f_obj)
+print(f'В файл {file_name} успешно записана строка')
+
+try:
+    with open(file_name, 'r') as f_obj:
+        int_list = [int(el) for el in f_obj.readline().split(' ')]
+except FileNotFoundError:
+    print(f'Ошибка: файл {file_name} не найден!')
+except IOError:
+    print(f'Ошибка чтения файла {file_name}!')
+finally:
+    print(f'Сумма чисел в строке: {sum(int_list)}')
--- lesson_5/quest_6.py
+++ lesson_5/quest_6.py
@@ -7,3 +7,36 @@
 Физкультура: ​ — 30(пр) —
 Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 """
+
+
+file_name = 'quest_6.txt'
+my_dict = {}
+""" Чтение данных из файла """
+try:
+    with open(file_name, 'r') as f_obj:
+        for line in f_obj:
+            key = ''
+            sum_val = 0
+            char_ind = line.find(':')
+
+            if char_ind + 1:
+                key = line[:char_ind]
+            else:
+                print(f'Не найден предмет в строке {line}')
+                continue
+            tmp_list = line[char_ind + 1:].split(' ')
+
+            for itr in tmp_list[1:]:
+                char_ind = itr.find('(')        # если не найдено - возвращает -1
+                if char_ind + 1:                # -1 + 1 = 0
+                    try:
+                        sum_val += int(itr[0:char_ind])
+                    except ValueError:
+                        print(f'Ошибка преобразования в строке {itr}')
+            my_dict.setdefault(key, sum_val)
+except FileNotFoundError:
+    print(f'Ошибка: файл {file_name} не найден!')
+except IOError:
+    print(f'Ошибка чтения файла {file_name}!')
+finally:
+    print(f'Данные успешно прочитаны из файла {file_name}\n{my_dict}')
